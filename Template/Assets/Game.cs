5c5e4e11b24321ab13f15159aab42b2ce23382fc
Jun-robot <saijun721.1@gmail.com>
2024-04-23 01:46:59 +0900

    Templateを作った

--- Template/Assets/Game.cs
+++ Template/Assets/Game.cs
@@ -15,10 +15,8 @@ public sealed class Game : GameBase{
 	/// <summary>
 	/// 初期化処理
 	/// </summary>
-	public override void InitGame()
-	{
-		// キャンバスの大きさを設定します
-		gc.ChangeCanvasSize(720, 1280);
+	public override void InitGame(){
+		
 	}
 
 	/// <summary>
@@ -27,25 +25,13 @@ public sealed class Game : GameBase{
 	public override void UpdateGame(){
 		// 起動からの経過時間を取得します
 		sec = (int)gc.TimeSinceStartup;
+
 	}
 
 	/// <summary>
 	/// 描画の処理
 	/// </summary>
 	public override void DrawGame(){
-		// 画面を白で塗りつぶします
-		gc.ClearScreen();
 
-		// 青空の画像を描画します
-		gc.DrawImage(GcImage.BlueSky, 0, 0);
-
-		// 黒の文字を描画します
-		gc.SetColor(0, 0, 0);
-		gc.SetFontSize(48);
-		gc.SetStringAnchor(GcAnchor.UpperLeft);
-		gc.DrawString("この文字と青空の画像が", 40, 160);
-		gc.DrawString("見えていれば成功です", 40, 270);
-		gc.SetStringAnchor(GcAnchor.UpperRight);
-		gc.DrawString($"{sec}s", 630, 10);
 	}
 }
