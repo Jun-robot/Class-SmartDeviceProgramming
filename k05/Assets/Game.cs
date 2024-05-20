0e5de6cd128f8a9ed7d812ac8ace825216a751ee
Jun-robot <saijun721.1@gmail.com>
2024-05-20 16:52:30 +0900

    第5回提出

--- k05/Assets/Game.cs
+++ k05/Assets/Game.cs
@@ -10,28 +10,48 @@ using UnityEngine.InputSystem;
 /// </summary>
 public sealed class Game : GameBase{
 	// 変数の宣言
-	int sec = 0;
+	int time = 600;
+	int score = 0;
 
 	/// <summary>
 	/// 初期化処理
 	/// </summary>
 	public override void InitGame(){
-		
+		gc.SetResolution(720, 1280);
 	}
 
 	/// <summary>
 	/// 動きなどの更新処理
 	/// </summary>
 	public override void UpdateGame(){
-		// 起動からの経過時間を取得します
-		sec = (int)gc.TimeSinceStartup;
-
+		time = time - 1;
+		for(int i=0 ; i< gc.PointerCount ; i++){
+			if(gc.GetPointerFrameCount(i)==1){
+				if(time >= 0){
+					score = score + 1;
+				} 
+			}
+		}
+		if(gc.GetPointerDuration(0) >= 2.0f && time < -120){
+			time =600;
+			score =0;
+		}
 	}
 
 	/// <summary>
 	/// 描画の処理
 	/// </summary>
 	public override void DrawGame(){
-
+		gc.ClearScreen();
+		gc.DrawImage(GcImage.BlueSky, 0, 0);
+		gc.SetColor(0, 0, 0);
+		gc.SetFontSize(36);
+		if(time >= 0 ){
+			gc.DrawString("time:"+time,60,160);
+		}
+		else {
+			gc.DrawString("finished!!",60,160);
+		}
+		gc.DrawString("score:"+score,60,200);
 	}
 }
